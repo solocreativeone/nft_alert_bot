e3191f422223c6a403f0529113f89510da9c871b
Your Name <solomondamagnifico@gmail.com>
2026-09-22 17:47:00 +0100

    chore: add GoPlus app credentials

--- config.py
+++ config.py
@@ -10,6 +10,8 @@ OPENSEA_API_KEY = os.environ.get("OPENSEA_API_KEY")
 GEMINI_API_KEY  = os.environ.get("GEMINI_API_KEY")
 HONEYPOT_API_KEY = os.environ.get("HONEYPOT_API_KEY")
 GOPLUS_API_KEY = os.environ.get("GOPLUS_API_KEY")
+GOPLUS_APP_KEY = os.environ.get("GOPLUS_APP_KEY") or GOPLUS_API_KEY
+GOPLUS_APP_SECRET = os.environ.get("GOPLUS_APP_SECRET")
 
 # Arc EVM RPC endpoint (Chain ID 5042). Configurable via ARC_RPC_URL or ARC_RPC.
 ARC_RPC_URL = os.environ.get("ARC_RPC_URL") or os.environ.get("ARC_RPC", "https://rpc.mainnet.arc.io")
